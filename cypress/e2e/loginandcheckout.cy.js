435745c21596cf1bb9ed0b2ade9d85bb6ab5f8ce
Ansluim <Abhishekparajuli33@gmail.com>
2025-05-15 22:11:06 +0545

    feat:fetch usrname from website

--- cypress/e2e/loginandcheckout.cy.js
+++ cypress/e2e/loginandcheckout.cy.js
@@ -4,7 +4,16 @@ describe('template spec', () => {
   it('valid username and password', () => {
     loginandcheckout
     .visiturl()
-    .enterusernameandpassword();
+    cy.get('.login_credentials_wrap-inner > .login_credentials')
+      .invoke('html')
+      .then((innerHtml)=>{
+        const username= innerHtml.split('<br>').map(str=>str.replace(/<[^>]+>/g, '').trim()).filter(Boolean)
+        cy.wrap(username).as('username')
+    })
+    cy.get('@username').then((username)=>{
+      cy.log('Listed username',username)
+    })
+    loginandcheckout.enterusernameandpassword(); 
     cy.url().then(url => {  
       if (url.includes('/inventory.html')) {
         loginandcheckout
